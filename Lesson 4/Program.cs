c73feb9b2613ed73e247703b964308c046c1a6c6
Константин Головин <Kestsssr@gmail.com>
2023-09-24 19:40:19 +0200

    Добавьте файлы проекта.

---
+++ Lesson 4/Program.cs
@@ -0,0 +1,27 @@
+﻿//1
+{
+    Console.WriteLine("Two roads diverged in a yellow wood,");
+    Console.WriteLine("And sorry I could not travel both");
+    Console.WriteLine("And be one traveler, long I stood");
+    Console.WriteLine("And looked down one as far as I could");
+    Console.WriteLine("To where it bent in the undergrowth.");
+}
+
+//2
+{
+    int a = 1;
+    int b = 13;
+    int c = 49;
+    Console.WriteLine($"{a} {b} {c}");
+}
+
+//3
+{
+    Console.WriteLine("Введите первое число:");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите второе число:");
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите третье число:");
+    int c = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{a}  {b}  {c}");
+}
